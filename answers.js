d3a525b439eb1f5d96d0b070dfd60d462868a951
Catherine <ducharme.catherine@gmail.com>
2015-09-11 18:10:11 +0000

    Created a function that returns the last character of a string

--- answers.js
+++ answers.js
@@ -1,6 +1,11 @@
-//This function takes a string as argument and returns the first character of the string.
+//This function takes a string as argument and returns its first character.
 
 function firstCharacter(string) {
     return string[0];
 }
 
+//This function takes a string as argument and returns its last character. 
+
+function lastCharacter(string) {
+    return string [ string.length - 1 ];
+}
